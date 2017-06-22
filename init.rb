e2afa5028d1cd2e4d5c6f0c4ddd6420d595fc38a
WST <ilya.averkov@gmail.com>
2017-06-22 15:52:49 +0300

    Тестовая первая версия

---
+++ init.rb
@@ -0,0 +1,18 @@
+
+require 'redmine'
+require 'open-uri'
+
+Redmine::Plugin.register :redmine_wiki_html_util do
+	name 'Redmine HTML macro'
+	author 'Ilya Averkov'
+	author_url 'https://github.com/WST/redmine-html-macro'
+	description 'A simple plugin allowing to use HTML blocks in the wiki pages'
+	version '0.0.1'
+
+	Redmine::WikiFormatting::Macros.register do
+		desc "Embed raw html\nUsage:\n<pre>{{html\nHTML CODE\n}}</pre>"
+		macro :html do |obj, args, text|
+			return text.html_safe
+		end 
+	end  
+end
